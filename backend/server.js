d0861506819e3575435f94660eedd73660dfd712
DanBilic <bilic90@gmx.de>
2020-11-21 20:29:21 +0100

    environment variables added

--- backend/server.js
+++ backend/server.js
@@ -1,5 +1,13 @@
 const express = require("express");
+const dotenv = require("dotenv");
+
+dotenv.config();
 
 const app = express();
 
-app.listen(5000, console.log("Server running on port 5000"));
+const PORT = process.env.PORT || 5000;
+
+app.listen(
+  PORT,
+  console.log(`Server running in ${process.env.NODE_ENV} mode on port ${PORT}`)
+);
