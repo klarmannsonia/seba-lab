7b3d8132dd176b4af1fe37c4c2de2efd3989ddd9
DanBilic <bilic90@gmx.de>
2020-11-21 19:43:42 +0100

    backend init + frontend proxy added

--- frontend/src/components/Footer.js
+++ frontend/src/components/Footer.js
@@ -2,8 +2,6 @@ import React from "react";
 import { makeStyles } from "@material-ui/core/styles";
 import Grid from "@material-ui/core/Grid";
 import Container from "@material-ui/core/Container";
-import Typography from "@material-ui/core/Typography";
-import Paper from "@material-ui/core/Paper";
 
 const useStyles = makeStyles((theme) => ({
   textCenter: {
