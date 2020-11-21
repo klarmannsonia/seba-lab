946a1e26442312b9c452780e274a015b0e089cc3
DanBilic <bilic90@gmx.de>
2020-11-21 21:28:48 +0100

    User model created

---
+++ backend/models/User.js
@@ -0,0 +1,32 @@
+import mongoose from "mongoose";
+import { stringify } from "querystring";
+
+const userSchema = mongoose.Schema(
+  {
+    name: {
+      type: String,
+      required: true,
+    },
+    email: {
+      type: String,
+      required: true,
+      unique: true, // only one email per person
+    },
+    password: {
+      type: String,
+      required: true,
+    },
+    isAdmin: {
+      type: Boolean,
+      required: true,
+      default: false,
+    },
+  },
+  {
+    timestamps: true,
+  }
+);
+
+const User = mongoose.model("User", userSchema);
+
+export default User;
