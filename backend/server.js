535ef393f57e4e9b69edd696699fb01a64c0e459
DanBilic <bilic90@gmx.de>
2020-11-22 10:29:31 +0100

    custom error handling with errormiddleware

--- backend/server.js
+++ backend/server.js
@@ -1,6 +1,7 @@
 import express from "express";
 import dotenv from "dotenv";
 import colors from "colors";
+import { notFound, errorHandler } from "./middleware/errorMiddleware.js";
 import connectDB from "./config/db.js";
 
 dotenv.config();
@@ -8,6 +9,12 @@ dotenv.config();
 connectDB();
 
 const app = express();
+
+// fallback endpoint for every request with no matching endpoint
+app.use(notFound);
+
+// overwrite default express error handler
+app.use(errorHandler);
 
 const PORT = process.env.PORT || 5000;
 
