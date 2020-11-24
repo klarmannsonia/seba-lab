ae016dc0cc79e224aa13006c9fcd0f9ce5c56c2f
DanBilic <bilic90@gmx.de>
2020-11-24 01:36:43 +0100

    swagger setup

--- backend/server.js
+++ backend/server.js
@@ -4,15 +4,47 @@ import colors from "colors";
 import { notFound, errorHandler } from "./middleware/errorMiddleware.js";
 import connectDB from "./config/db.js";
 
+//swagger
+import swaggerJsDoc from "swagger-jsdoc";
+import swaggerUi from "swagger-ui-express";
+
 // import routes
 import userRoutes from "./routes/userRoutes.js";
 
 dotenv.config();
-
 connectDB();
 
+// swagger setup
+const swaggerOptions = {
+  definition: {
+    info: {
+      title: "KELVIN API",
+      description: "KELVIN API Information",
+      version: "1.0.0",
+      license: {
+        name: "MIT",
+        url: "https://spdx.org/licenses/MIT.html",
+      },
+      contact: {
+        name: "Daniel Bilic",
+        email: "danielbilic90@gmail.com",
+      },
+    },
+    servers: [
+      {
+        url: "http://localhost:5000/api",
+      },
+    ],
+  },
+  apis: ["./routes/userRoutes.js"],
+};
+
+const swaggerDocs = swaggerJsDoc(swaggerOptions);
+
+// express init
 const app = express();
 app.use(express.json());
+app.use("/api/docs", swaggerUi.serve, swaggerUi.setup(swaggerDocs));
 
 app.get("/", (req, res) => {
   res.send("API is running ...");
